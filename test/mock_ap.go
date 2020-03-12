c02daa78ccb29afb71c93d2bf4696591662cbda1
Yusuke Nojima <nojima@ynojima.com>
2020-03-12 16:54:18 +0900

    Add TestReverseProxy

---
+++ test/mock_ap.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"sync"
+	"testing"
+)
+
+type MockAP struct {
+	server *http.Server
+
+	host string
+	port int
+
+	exited chan error // server が終了したらその err が入る
+
+	lastRequest *http.Request
+	mutex       sync.Mutex
+}
+
+func StartMockAP(t *testing.T) *MockAP {
+	host := os.Getenv("TESTER_NAME")
+	if host == "" {
+		t.Fatal("Please specify TESTER_NAME")
+	}
+
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ap := &MockAP{
+		host:   host,
+		port:   l.Addr().(*net.TCPAddr).Port,
+		exited: make(chan error),
+	}
+	ap.server = &http.Server{
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			ap.mutex.Lock()
+			defer ap.mutex.Unlock()
+			ap.lastRequest = req
+			w.Write([]byte("I am AP server"))
+		}),
+	}
+
+	go func() {
+		if err := ap.server.Serve(l); err != nil && err != http.ErrServerClosed {
+			t.Log(err)
+		}
+		ap.exited <- err
+		close(ap.exited)
+	}()
+
+	return ap
+}
+
+func (a *MockAP) Address() string {
+	return fmt.Sprintf("%s:%d", a.host, a.port)
+}
+
+func (a *MockAP) Close(t *testing.T) {
+	if err := a.server.Close(); err != nil {
+		t.Log(err)
+	}
+
+	<-a.exited // 終了を待つ
+}
--- test/nginx_test.go
+++ test/nginx_test.go
@@ -1,134 +1,16 @@
 package main
 
 import (
-	"crypto/rand"
-	"encoding/base32"
-	"fmt"
 	"io/ioutil"
 	"net/http"
-	"os"
-	"os/exec"
-	"strings"
 	"testing"
-	"time"
 )
 
-//-----------------------------------------------------------------------------
-
-type Nginx struct {
-	cmd           *exec.Cmd
-	containerName string
-	exited        chan int // 終了したら ExitCode が入る
-}
-
-type NginxConfig struct {
-	APServerAddress string
-}
-
-// StartNginx は新しい nginx のインスタンスを起動する。
-func StartNginx(t *testing.T, config NginxConfig) *Nginx {
-	name := "nginx-" + randomSuffix()
-
-	network := os.Getenv("DOCKER_NETWORK")
-	if network == "" {
-		t.Fatal("Please specify DOCKER_NETWORK")
-	}
-
-	if config.APServerAddress == "" {
-		config.APServerAddress = "localhost:9999" // 無効なポート
-	}
-
-	args := []string{
-		"run", "--rm",
-		"--name", name,
-		"--net", network,
-		"-e", fmt.Sprintf("AP_SERVER_ADDR=%s", config.APServerAddress),
-		"sample-nginx:latest",
-	}
-	cmd := exec.Command("docker", args...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stdout
-	if err := cmd.Start(); err != nil {
-		t.Fatal(err)
-	}
-
-	nginx := &Nginx{
-		cmd:           cmd,
-		containerName: name,
-		exited:        make(chan int),
-	}
-
-	go func() {
-		if err := cmd.Wait(); err != nil {
-			t.Log(err)
-		}
-		nginx.exited <- cmd.ProcessState.ExitCode()
-		close(nginx.exited)
-	}()
-
-	return nginx
-}
-
-// URL はこの nginx にアクセスするための URL を返す。
-func (n *Nginx) URL() string {
-	return fmt.Sprintf("http://%s:80", n.containerName)
-}
-
-// Wait はこの nginx が起動するまで待つ。
-func (n *Nginx) Wait(t *testing.T) {
-	maxRetry := 20
-	for i := 0; i < maxRetry; i++ {
-		t.Logf("Wait for nginx... (%d/%d)", i, maxRetry)
-
-		select {
-		case exitCode := <- n.exited:
-			t.Fatalf("nginx exited unexpectedly: exitCode=%d", exitCode)
-		default:
-		}
-
-		resp, err := http.Get(n.URL() + "/health")
-		if err != nil {
-			time.Sleep(500 * time.Millisecond)
-			continue
-		}
-		resp.Body.Close()
-		return
-	}
-}
-
-// Close はこの nginx を終了する。
-func (n *Nginx) Close(t *testing.T) {
-	cmd := exec.Command("docker", "kill", n.containerName)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stdout
-	if err := cmd.Run(); err != nil {
-		t.Fatal(err)
-	}
-
-	<-n.exited // 終了するまで待つ
-}
-
-// randomSuffix はコンテナ名やボリューム名の suffix として使うためのランダム文字列を返す
-func randomSuffix() string {
-	b := make([]byte, 4)
-	_, err := rand.Read(b)
-	if err != nil {
-		panic(err)
-	}
-	enc := base32.StdEncoding.WithPadding(base32.NoPadding)
-	return strings.ToLower(enc.EncodeToString(b))
-}
-
-
-//-----------------------------------------------------------------------------
-
-func TestRobotsTxt(t *testing.T) {
+func TestStaticResponses(t *testing.T) {
 	t.Parallel()
 
 	nginx := StartNginx(t, NginxConfig{})
-	t.Cleanup(func() {
-		nginx.Close(t)
-	})
+	defer nginx.Close(t)
 	nginx.Wait(t)
 
 	t.Run("GET /robots.txt", func(t *testing.T) {
@@ -161,6 +43,40 @@ func TestRobotsTxt(t *testing.T) {
 
 		if resp.StatusCode != http.StatusForbidden {
 			t.Errorf("status code should be 400, but %d", resp.StatusCode)
+		}
+	})
+}
+
+func TestReverseProxy(t *testing.T) {
+	t.Parallel()
+
+	ap := StartMockAP(t)
+	defer ap.Close(t)
+
+	nginx := StartNginx(t, NginxConfig{
+		APServerAddress: ap.Address(),
+	})
+	defer nginx.Close(t)
+	nginx.Wait(t)
+
+	t.Run("GET /", func(t *testing.T) {
+		resp, err := http.Get(nginx.URL() + "/")
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			t.Errorf("status code should be 200, but %d", resp.StatusCode)
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		expected := "I am AP server"
+		if string(body) != expected {
+			t.Errorf("unexpected response body: %s", string(body))
 		}
 	})
 }
